b32f16893e61fe05cada9b630d96fa84b81c2275
soskill71 <kartikjodhpur@gmail.com>
2019-10-13 21:33:03 +0530

    Add dials

--- run.py
+++ run.py
@@ -1,14 +1,12 @@
 import cv2
 import sys
-from PyQt5.QtWidgets import QWidget, QMessageBox, QApplication, QLabel, QGridLayout
+from PyQt5.QtWidgets import QWidget, QMessageBox, QApplication, QLabel, QDial
 from PyQt5.QtGui import QPixmap
 
 
 class Image(QWidget):
     def __init__(self,image_path):
         super().__init__()
-        #self.grid = QGridLayout()
-        #self.setLayout(self.grid)
         self.title = "Image Processing"
         self.left = 10
         self.top = 10
@@ -22,14 +20,20 @@ class Image(QWidget):
         self.setWindowTitle(self.title)
         self.setGeometry(self.left, self.top, self.width, self.height)
         
-        self.im = QPixmap(self.image_path)
-        self.im = self.im.scaled(260,260)
+        self.im = QPixmap(self.image_path).scaled(260,260)
         self.im_label = QLabel(self)
         self.im_label.setPixmap(self.im)
         self.im_label.move(120,40)
-        #self.grid.addWidget(self.im_label,0,1)
-        
 
+        self.dial1 = QDial(self)
+        self.dial2 = QDial(self)
+        self.dial1.setMinimum(0)
+        self.dial1.setMaximum(255)
+        self.dial2.setMinimum(0)
+        self.dial2.setMaximum(255)
+        self.dial1.move(20,420)
+        self.dial2.move(120,420)
+        
         self.show()
 
 
