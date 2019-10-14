abf9423a5ba669175e997c24bc2de7c26e985d64
soskill71 <kartikjodhpur@gmail.com>
2019-10-14 15:41:22 +0530

    Add combobox

--- run.py
+++ run.py
@@ -1,6 +1,6 @@
 import cv2
 import sys
-from PyQt5.QtWidgets import QWidget, QMessageBox, QApplication, QLabel, QDial
+from PyQt5.QtWidgets import QWidget, QMessageBox, QApplication, QLabel, QDial, QComboBox
 from PyQt5.QtGui import QPixmap
 
 
@@ -13,26 +13,32 @@ class Image(QWidget):
         self.width = 500
         self.height = 680
         self.image_path = image_path
+        self.dial1 = QDial(self)
+        self.dial2 = QDial(self)
+        self.im = QPixmap(self.image_path).scaled(260,260)
+        self.im_label = QLabel(self)
+        self.cb1 = QComboBox(self)
+        self.cb2 = QComboBox(self)
         self.initUI()
 
 
     def initUI(self):
         self.setWindowTitle(self.title)
         self.setGeometry(self.left, self.top, self.width, self.height)
-        
-        self.im = QPixmap(self.image_path).scaled(260,260)
-        self.im_label = QLabel(self)
+                
         self.im_label.setPixmap(self.im)
         self.im_label.move(120,40)
 
-        self.dial1 = QDial(self)
-        self.dial2 = QDial(self)
         self.dial1.setMinimum(0)
         self.dial1.setMaximum(255)
         self.dial2.setMinimum(0)
         self.dial2.setMaximum(255)
         self.dial1.move(20,420)
         self.dial2.move(120,420)
+
+        self.cb1.addItems(["BINARY", "BINARY_INV","TRUNC","TOZERO","TOZERO_INV"])
+        self.cb1.move(40,380)
+        self.cb2.move(280,380)
         
         self.show()
 
