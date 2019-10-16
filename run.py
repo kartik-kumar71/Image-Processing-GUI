746c2d10ab9b6f4f08cbcd9b460de4151e28a20c
soskill71 <kartikjodhpur@gmail.com>
2019-10-16 21:18:41 +0530

    Implement dial1 and dial2 functionality

--- run.py
+++ run.py
@@ -12,16 +12,15 @@ class Image(QWidget):
         self.width = 500
         self.height = 680
         self.image_path = image_path
-        image = cv2.imread(self.image_path)
-        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-        qim = QImage(gray.data,gray.shape[1],gray.shape[0],QImage.Format_Grayscale8)
+        self.image = cv2.imread(self.image_path)
+        self.image_gray = cv2.cvtColor(self.image, cv2.COLOR_BGR2GRAY)
         self.dial1 = QDial(self)
         self.dial2 = QDial(self)
-        self.im = QPixmap(qim).scaled(260,260)
         self.im_label = QLabel(self)
         self.cb1 = QComboBox(self)
         self.cb2 = QComboBox(self)
         self.reset_button = QPushButton("Revert changes",self)
+        self.updateImage(self.image_gray)
         self.initUI()
 
 
@@ -29,8 +28,8 @@ class Image(QWidget):
         self.setWindowTitle(self.title)
         self.setGeometry(self.left, self.top, self.width, self.height)
                 
-        self.im_label.setPixmap(self.im)
-        self.im_label.move(120,40)
+        #self.im_label.setPixmap(self.im)
+        #self.im_label.move(120,40)
 
         self.dial1.setMinimum(0)
         self.dial1.setMaximum(255)
@@ -38,6 +37,8 @@ class Image(QWidget):
         self.dial2.setMaximum(255)
         self.dial1.move(20,420)
         self.dial2.move(120,420)
+        self.dial1.valueChanged.connect(self.dial1_changed)
+        self.dial2.valueChanged.connect(self.dial2_changed)
 
         self.cb1.addItems(["BINARY", "BINARY_INV","TRUNC","TOZERO","TOZERO_INV"])
         self.cb1.move(40,380)
@@ -45,7 +46,25 @@ class Image(QWidget):
 
         self.reset_button.move(200,600)
         
+        
         self.show()
+
+    def updateImage(self,gray):
+        """
+        qim = QImage(gray.data,gray.shape[1],gray.shape[0],gray.shape[1]*3,QImage.Format_RGB888)
+        self.im = QPixmap(qim).scaled(260,260)
+        self.im_label.setPixmap(self.im)
+        """
+        cv2.imshow('image',gray)
+
+    def dial1_changed(self):
+        _, temp = cv2.threshold(self.image_gray,self.dial1.value(),self.dial2.value(),cv2.THRESH_BINARY)
+        self.updateImage(temp)
+    def dial2_changed(self):
+        _, temp = cv2.threshold(self.image_gray,self.dial1.value(),self.dial2.value(),cv2.THRESH_BINARY)
+        self.updateImage(temp)
+
+
 
 
 
